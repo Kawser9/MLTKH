74839a5b29aacdd56fec1f990469f2b432ebf115
kawser.hossain <18103210kawser@gmail.com>
2023-06-03 10:56:44 +0600

    MOdified on frontend

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// Route::get('/home', function(){
+//     return view('frontend.layouts.master');
+
+// });
+Route::get('/home', [HomeController::class, 'home'])->name('home');
+
